63df90231c2a1a71526cbb95c9f4d25fee62a5c7
Bill Chung <bill_chungiii@hotmail.com>
2024-05-19 19:17:51 +0800

    完成 view 與 viewModel

---
+++ EnancedWrapPanelSample001/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using BindingLibrary;
+using EnancedWrapPanelSample001.Panels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EnancedWrapPanelSample001
+{
+    public class MainViewModel : NotifyPropertyBase
+    {
+        private ObservableCollection<string> _items;
+        public ObservableCollection<string> Items
+        {
+            get { return _items; }
+            set { SetProperty(ref _items, value); }
+        }
+
+     
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();          
+        }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new RelayCommand((x) =>
+                {
+                    Items.Add($" == Item {Items.Count} ==");
+                });
+            }
+        }
+
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return new RelayCommand((x) =>
+                {
+                    Items.Clear();
+                });
+            }
+        }
+    }
+}
